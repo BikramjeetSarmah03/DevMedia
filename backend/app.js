59be169ce28dc2d5ffba8cd4f335fffba4daa66c
Bikramjeet Sarmah <9854bikram@gmail.com>
2023-02-06 12:05:15 +0530

    CORS

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const errorMiddleware = require("./middlewares/error");
 const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,6 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(fileUpload());
+app.use(
+  cors({
+    origin: "https://devmedia-frontend.onrender.com/",
+  })
+);
 
 if (process.env.NODE_ENV != "production") {
   require("dotenv").config({ path: "config/config.env" });
