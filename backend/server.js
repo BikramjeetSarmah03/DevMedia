790b899ca937781d29c23613e5abb9fff982ab05
Bikramjeet Sarmah <9854bikram@gmail.com>
2023-02-06 11:48:19 +0530

    Cors Fix

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,8 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   // pingTimeout: 60000,
   cors: {
-    origin: "https://devmedia-frontend.onrender.com",
-    // origin: "*",
+    // origin: "https://devmedia-frontend.onrender.com",
+    origin: "*",
   },
 });
 
